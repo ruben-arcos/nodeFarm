22721605ed4d343f9b1415df1ee55c190d4851b4
Ruben Arcos <ruben.arcos06@gmail.com>
2023-12-07 23:39:45 -0600

    created modules folder and file, added notes.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const replaceTemplate = require('./modules/replaceTemplate')
 //////////////////////////////////////////////////////////////////////////////////////////////////// FILES
 
 // Blocking synchronous way excercise
@@ -41,21 +42,6 @@ const url = require("url");
 // console.log("Will read file!!");
 
 //////////////////////////////////////////////////////////////////////////////////////////////////// SERVER
-
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
 
 // __dirname is where the current file is located
 // Top level code is only executed once, sync will only work since it will not be blocked
